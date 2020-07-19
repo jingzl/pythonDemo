cbe5ea6043e4bfacae3ce82e882e2fc76139472b
jingzl <jingzl@hotmail.com>
2020-07-19 22:16:06 +0800

    医生信息爬取 init

---
+++ scrapyDemo/medicalSpider/hospitalSpider.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 2020
+爬取医院医生及排班信息
+"""
+__author__ = 'jingzl'
+__version__ = '1.0'
+
+
+def main():
+    print("hello")
+    url = "https://www.tjh.com.cn/Menzhen/Arrange1.aspx?week=0&yuanqu=0&haobie=0&riqi=20200718&ksdaima=#title"
+    url = "https://www.tjh.com.cn/Menzhen/Arrange1.aspx?week=1&yuanqu=0&haobie=0&riqi=20200719&ksdaima=#title"
+
+    url = "https://www.tjh.com.cn/Menzhen/ZuanJia_details.aspx?id=302001&mzDaima=#title"
+    url = "https://www.tjh.com.cn/Menzhen/ZuanJia_details.aspx?id=851001&mzDaima=#title"
+
+
+    url = "http://www.whuh.com/doctorss/search.html"
+    url = "http://www.whuh.com/doctorss/index/is_doc1/1.html"
+    url = "http://www.whuh.com/help/menzheng.html"
+
+
+
+
+    return
+
+
+if __name__ == '__main__':
+    main()
