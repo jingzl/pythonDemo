7095d3b0834a6cf8822559875350188157adf682
jingzl <jingzl@hotmail.com>
2020-09-03 23:16:23 +0800

    完成数据的提取及存储

--- excelTrans/exceltrans.py
+++ excelTrans/exceltrans.py
@@ -6,7 +6,8 @@ Excel文件信息转换
 __author__ = 'jingzl'
 __version__ = '1.0'
 
-
+import pandas as pd
+import numpy as np
 import xlrd
 
 CONST_DATA_PATH = './data/'
@@ -14,8 +15,9 @@ CONST_OUTPUT_PATH = './output/'
 
 
 def saveFile(df_zz, df_tz, df_fz):
-
-    pass
+    df_zz.to_excel(CONST_OUTPUT_PATH + '_1.xlsx', index=False)
+    df_tz.to_excel(CONST_OUTPUT_PATH + '_2.xlsx', index=False)
+    df_fz.to_excel(CONST_OUTPUT_PATH + '_3.xlsx', index=False)
 
 
 def parseSheet(sheet):
@@ -25,26 +27,55 @@ def parseSheet(sheet):
     fz_ls = []
     diseaseName = sheet.cell_value(0, 1)
     print(diseaseName)
+    flag = 0
+    lastName = ''
     for i in range(sheet.nrows):
         row = sheet.row(i)
-        # 症状名称
-        if row[0].value == '症状名称' and row[1].value == '症状属性值':
-            print(row[0].value)
+        name = row[0].value.strip()
+        val = row[1].value.strip()
+        if len(name) <= 0 and len(val) <= 0:
+            continue
+        if name == '症状名称' and val == '症状属性值':
+            flag = 1
+            lastName = ''
+        elif name == '体征名称' and val == '体征属性值':
+            flag = 2
+            lastName = ''
+        elif name == '辅助检查名称' and val == '特征值':
+            flag = 3
+            lastName = ''
 
-        # 体征名称
-        if row[0].value == '体征名称' and row[1].value == '体征属性值':
-            print(row[0].value)
+        if flag == 1 and name != '症状名称':
+            if len(lastName) <= 0 and len(name) <= 0:
+                print('data error')
+                continue
+            if len(name) > 0 and len(val) > 0:
+                zz_ls.append([diseaseName, name, val])
+                lastName = name
+            if len(name) <= 0 and len(val) > 0 and len(lastName) > 0:
+                zz_ls.append([diseaseName, lastName, val])
+        elif flag == 2 and name != '体征名称':
+            if len(lastName) <= 0 and len(name) <= 0:
+                print('data error')
+                continue
+            if len(name) > 0 and len(val) > 0:
+                tz_ls.append([diseaseName, name, val])
+                lastName = name
+            if len(name) <= 0 and len(val) > 0 and len(lastName) > 0:
+                tz_ls.append([diseaseName, lastName, val])
+        elif flag == 3 and name != '辅助检查名称':
+            if len(lastName) <= 0 and len(name) <= 0:
+                print('data error')
+                continue
+            if len(name) > 0 and len(val) > 0:
+                fz_ls.append([diseaseName, name, val])
+                lastName = name
+            if len(name) <= 0 and len(val) > 0 and len(lastName) > 0:
+                fz_ls.append([diseaseName, lastName, val])
 
-
-        # 辅助检查名称
-        if row[0].value == '辅助检查名称' and row[1].value == '特征值':
-            print(row[0].value)
-
-
-
-
-    # df_zz =
-
+    df_zz = pd.DataFrame(zz_ls, columns=['疾病名称', '症状名称', '症状属性值'], index=np.arange(len(zz_ls)))
+    df_tz = pd.DataFrame(tz_ls, columns=['疾病名称', '体征名称', '体征属性值'], index=np.arange(len(tz_ls)))
+    df_fz = pd.DataFrame(fz_ls, columns=['疾病名称', '辅助检查名称', '辅助检查属性值'], index=np.arange(len(fz_ls)))
     return df_zz, df_tz, df_fz
 
 
